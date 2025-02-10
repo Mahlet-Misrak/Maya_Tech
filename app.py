35eeb8a60afb6accf325a3efcf2f0442a4502084
Mahlet Argaw <39273757+Mahlet-Misrak@users.noreply.github.com>
2025-02-10 13:12:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
+import os 
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_mail import Mail, Message
+
+load_dotenv()
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
-
-# Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mahletargaw1@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dcur vnqb aplq pclv'
-
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 
